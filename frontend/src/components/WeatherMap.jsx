ac3a78995318ec6c4882223b16117d3b18326e8c
Garapati Janardhan swamy <gjs7s3p@gmail.com>
2026-09-09 16:27:06 +0530

    fix map tiles

--- frontend/src/components/WeatherMap.jsx
+++ frontend/src/components/WeatherMap.jsx
@@ -52,10 +52,12 @@ export default function WeatherMap({ events = [], height = '500px', showLegend =
         zoomControl={true}
         scrollWheelZoom={true}
       >
+       
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}{r}.png"
         />
+
         <FitBounds events={events} />
         {events.map((event) => {
           if (!event.latitude || !event.longitude) return null;
