f3eefb0b46befb54c518f21b544d1c8a6a448cf5
Garapati Janardhan swamy <gjs7s3p@gmail.com>
2026-09-09 17:26:55 +0530

    switch to OpenStreetMap tiles

--- frontend/src/components/WeatherMap.jsx
+++ frontend/src/components/WeatherMap.jsx
@@ -52,10 +52,11 @@ export default function WeatherMap({ events = [], height = '500px', showLegend =
         zoomControl={true}
         scrollWheelZoom={true}
       >
-       
+      
+
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         <FitBounds events={events} />
